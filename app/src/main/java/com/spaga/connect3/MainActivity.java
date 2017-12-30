4aece1196422dbced77365ecb08ab397540cfb43
spagabtti <spagabtti@gmail.com>
2017-12-29 20:38:00 -0800

    edit

--- app/src/main/java/com/spaga/connect3/MainActivity.java
+++ app/src/main/java/com/spaga/connect3/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,9 +36,13 @@ public class MainActivity extends AppCompatActivity {
                 if (gameState[winningPosition[0]] == gameState[winningPosition[1]] &&
                         gameState[winningPosition[1]] == gameState[winningPosition[2]] &&
                         gameState[winningPosition[0]] != 2) {
-                    System.out.println(gameState[winningPosition[0]]);
-
                     // Someone won
+                    String winner = "Red";
+                    if (gameState[winningPosition[0]] == 0)
+                        winner = "Yellow";
+
+                    TextView winnerMsg = findViewById(R.id.winnerMsg);
+                    winnerMsg.setText(winner + " has won");
 
                     LinearLayout layout = findViewById(R.id.playAgainLayout);
                     layout.setVisibility(View.VISIBLE);
