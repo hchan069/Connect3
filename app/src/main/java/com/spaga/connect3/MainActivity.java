916a2145a4eb357bf435bf5f7fb1ed955f7ae14b
spagabtti <spagabtti@gmail.com>
2017-12-29 19:24:44 -0800

    edit

--- app/src/main/java/com/spaga/connect3/MainActivity.java
+++ app/src/main/java/com/spaga/connect3/MainActivity.java
@@ -7,11 +7,23 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    // 0 = yellow, 1 = red
+    int activePlayer = 0;
+
     public void dropIn(View view) {
         ImageView counter = (ImageView) view;
 
         counter.setTranslationY(-1500f);
-        counter.setImageResource(R.drawable.yellow_token);
+
+        if (activePlayer == 0) {
+            counter.setImageResource(R.drawable.yellow_token);
+            activePlayer = 1;
+        }
+        else {
+            counter.setImageResource(R.drawable.red_token);
+            activePlayer = 0;
+        }
+
         counter.animate().translationYBy(1500f).setDuration(300);
     }
 
